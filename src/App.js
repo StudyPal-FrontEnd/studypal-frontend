296a5ac4ac4a163626f98bab356bb212fdeea638
Kinsammy <fanusamuel@gmail.com>
2023-05-19 17:12:39 +0100

    Dashboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LandingPage from './container/pages/components/auth/LandingPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './container/pages/components/auth/Login';
 import Signup from './container/pages/components/auth/SignUp';
+import Dashboard from './container/pages/components/dashboard/Dashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<LandingPage />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='dashboard' element={<Dashboard />} />
         </Routes>
     </Router>
    </div>
--- src/container/pages/components/auth/LandingPage.jsx
+++ src/container/pages/components/auth/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import brandImg from "../../../../assets/images/svg/brand.svg"
 import banner from "../../../../assets/images/svg/banner.svg"
-import '../../styles/landingPage.css';
+import styles from '../../styles/LandingPage.module.css';
 
 
 
@@ -10,29 +10,29 @@ import '../../styles/landingPage.css';
 const LandingPage = () => {
     const navigate = useNavigate(); 
     return (
-        <div className="section">
-            <header>
-                <div className="nav-brand">
-                    <img className="brand" src={brandImg} alt="brand" />
+        <div className={styles.section}>
+            <div className={styles.header}>
+                <div className={styles.navBrand}>
+                    <img className={styles.brand} src={brandImg} alt="brand" />
                 </div>
-                <nav>
+                <div className={styles.nav}>
                     <ul>
                         <li><Link to="/">About</Link></li>
                         <li><Link to="/">Offer</Link></li>
                         <li><Link to="/">Contact</Link></li>
                     </ul>
-                </nav>
-            </header>
-            <main>
-                <article>
+                </div>
+            </div>
+            <div className={styles.main}>
+                <div className={styles.article}>
                     <h3>StudyPal help organize, schedule and track studying activities and aid learning capacity.</h3>
-                    <button className="banner-btn" onClick={() => navigate('/login')}>Get Started</button>
-                </article>
-                <aside>
-                <img className="brand" src={banner} alt="banner" />
-                </aside>
+                    <button className={styles.pageButton} onClick={() => navigate('/login')}>Get Started</button>
+                </div>
+                <div className={styles.aside}>
+                <img className={styles.banner} src={banner} alt="banner" />
+                </div>
 
-            </main>
+            </div>
 
                 
 
